6b86e21ea757fe9125794c020d105e349dfd681e
EMILY_BATES <ebates@bbilogistics.com>
2024-03-13 14:34:15 -0400

    authentication added

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,22 @@
+'use client'
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
+  const { status, data: session} = useSession();
+
   return (
     <div className='flex bg-slate-200 p-5'>
       <Link className='mr-5' href='/'>Next.js</Link>
       <Link className='mr-5' href='/users'>Users</Link>
+      {status === 'loading' && <div>Loading...</div>}
+      {status === 'authenticated' && 
+      <div>
+        {session.user!.name}
+        <Link href="/api/auth/signout" className='ml-3'>Sign Out</Link>
+      </div>}
+      {status === 'unauthenticated' && <Link className='mr-5' href='/api/auth/signin'>Sign in</Link>}
     </div>
   )
 }
